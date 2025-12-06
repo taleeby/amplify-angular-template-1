396852e874724bb01de1ba9b3af0c4b3cd4e2f36
taleeby <37315048+taleeby@users.noreply.github.com>
2025-12-06 23:51:06 +1100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
